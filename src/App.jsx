0c1c4aa9ec06454f75e0f6a9a3f954f91d14fee1
Agustin Magana <agusmagana98@gmail.com>
2023-09-25 21:32:48 -0600

    fotos home

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter } from 'react-router-dom'
-import Header from './Components/Header'
-import RoutesIndex from './routes/RoutesIndex'
+import { AuthProvider } from '@/context/AuthContext'
+import Header from '@/Components/Header'
+import RoutesIndex from '@/routes/RoutesIndex'
+
+import './App.css'
 
 function App () {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <RoutesIndex />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Header />
+          <RoutesIndex />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
